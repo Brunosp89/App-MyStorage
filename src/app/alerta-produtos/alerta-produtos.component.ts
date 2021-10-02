284a4661761ddaed3e02fbf1bfaf6cc57947be5d
Brunosp89 <brunosp89@gmail.com>
2021-10-01 22:57:32 -0300

    2 files added

---
+++ src/app/alerta-produtos/alerta-produtos.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Produto } from '../produtos';
+@Component({
+  selector: 'app-alerta-produtos',
+  templateUrl: './alerta-produtos.component.html',
+  styleUrls: ['./alerta-produtos.component.css'],
+})
+export class AlertaProdutosComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
