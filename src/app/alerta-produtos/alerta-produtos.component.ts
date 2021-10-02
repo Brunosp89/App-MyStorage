12c29587479945c0931864b439e18b095d89ba43
Brunosp89 <brunosp89@gmail.com>
2021-10-01 23:13:08 -0300

    4 files modified

--- src/app/alerta-produtos/alerta-produtos.component.ts
+++ src/app/alerta-produtos/alerta-produtos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Produto } from '../produtos';
 
 @Component({
   selector: 'app-alerta-produtos',
