0ca4b1897feecd2660a9cb68a4b21afff2c36bf5
Brunosp89 <brunosp89@gmail.com>
2021-10-01 22:52:02 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { RouterModule } from '@angular/router';
 import { ListaProdutosComponent } from './lista-produtos/lista-produtos.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'lista-produtos',
-        component: ListaProdutosComponent,
-      },
-    ]),
-  ],
+  imports: [BrowserModule, FormsModule],
   declarations: [
     AppComponent,
     HelloComponent,
